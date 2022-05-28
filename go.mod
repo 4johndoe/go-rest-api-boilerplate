6c0ad74fb2ff9c323530218cebdb7f3b8c4ff295
bogdan demchenko <bogdandemchenko@gmail.com>
2022-05-28 13:17:39 +0300

    config struct methods

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.18
 
 require (
 	github.com/go-ozzo/ozzo-routing/v2 v2.4.0 // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/qiangxue/go-env v1.0.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/qiangxue/go-env"
+	"go-rest-api/pkg/log"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+)
+
 const (
 	defaultServerPort         = 8080
 	defaultJWTExpirationHours = 72
@@ -15,4 +23,42 @@ type Config struct {
 	JWTSigningKey string `yaml:"jwt_signing_key" env:"JWT_SIGNING_KEY,secret"`
 	// JWT expiration in hours. Defaults to 72 hours (3 days)
 	JWTExpiration int `yaml:"jwt_expiration" env:"JWT_EXPIRATION"`
+}
+
+// Validate validates the application configuration.
+func (c Config) Validate() error {
+	return validation.ValidateStruct(&c,
+		validation.Field(&c.DSN, validation.Required),
+		validation.Field(&c.JWTSigningKey, validation.Required),
+	)
+}
+
+// Load returns an application configuration which is populated from the given configuration file and environment variables.
+func Load(file string, logger log.Logger) (*Config, error) {
+	// default config
+	c := Config{
+		ServerPort:    defaultServerPort,
+		JWTExpiration: defaultJWTExpirationHours,
+	}
+
+	// load from YAML config file
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	if err = yaml.Unmarshal(bytes, &c); err != nil {
+		return nil, err
+	}
+
+	// load from environment variables prefixed with "APP_"
+	if err = env.New("APP_", logger.Infof).Load(&c); err != nil {
+		return nil, err
+	}
+
+	// validation
+	if err = c.Validate(); err != nil {
+		return nil, err
+	}
+
+	return &c, err
 }
