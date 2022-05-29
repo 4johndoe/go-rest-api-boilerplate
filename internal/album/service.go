df85ae6a1e575ae245f2ab9419c9ae7b430d53ff
bogdan demchenko <bogdandemchenko@gmail.com>
2022-05-29 16:21:38 +0300

    album service method mocks

--- internal/album/service.go
+++ internal/album/service.go
@@ -41,6 +41,34 @@ func (m UpdateAlbumRequest) Validate() error {
 }
 
 type service struct {
-	//repo   Repository todo
+	repo   Repository
 	logger log.Logger
+}
+
+func (s service) Get(ctx context.Context, id string) (Album, error) {
+	panic("implement me")
+}
+
+func (s service) Query(ctx context.Context, offset, limit int) ([]Album, error) {
+	panic("implement me")
+}
+
+func (s service) Count(ctx context.Context) (int, error) {
+	panic("implement me")
+}
+
+func (s service) Create(ctx context.Context, input CreateAlbumRequest) (Album, error) {
+	panic("implement me")
+}
+
+func (s service) Update(ctx context.Context, id string, input UpdateAlbumRequest) (Album, error) {
+	panic("implement me")
+}
+
+func (s service) Delete(ctx context.Context, id string) (Album, error) {
+	panic("implement me")
+}
+
+func NewService(repo Repository, logger log.Logger) Service {
+	return service{repo, logger}
 }
